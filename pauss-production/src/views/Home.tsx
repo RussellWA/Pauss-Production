807c4b03415bcf377aa3d1035e71b8a2407723d3
Russell <gamingrussellwa@gmail.com>
2025-04-20 00:09:20 +0700

    [Update] Home File, Index File | [Add] Word Transition file and css
    - Added Poppins font to index.html
    - New Typewriter effect at Home file
    - New Word Transition effect (needs fixing)

--- pauss-production/src/views/Home.tsx
+++ pauss-production/src/views/Home.tsx
@@ -1,11 +1,47 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import '../css/WordsTransition.css'
+import WordsTransition from '../components/FadingText/WordsTransition';
 
 const Home = () => {
+    // Typewriter
+    const [ firstDone, setFirstDone ] = useState(false);
+    const [ typedLength, setTypedLength ] = useState(0);
+    const firstText = "👋 Hi, we're "
+    
+    // CSS Transition
+    const words = ['websites', 'games', 'digital experiences'];
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [inProp, setInProp] = useState(true);
+
+    const handleType = () => {
+        setTypedLength(prev => {
+            const newLength = prev + 1;
+            if (newLength >= firstText.length) {
+                setFirstDone(true)
+            }
+            return newLength;
+        })
+    };
+
+    useEffect(() => {
+    const timer = setInterval(() => {
+        setInProp(false); // Start fade-out
+        setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+        setInProp(true); // Start fade-in
+        }, 500); // Match the transition duration in CSS
+    }, 2000);
+
+    return () => clearInterval(timer); // Clean up the interval on unmount
+    }, [words]);
+
     return (
         <Box
             display="flex"
-            justifyContent="flex-start"
+            justifyContent="center"
             alignItems="flex-start"
             sx={{
                 marginTop: 10
@@ -22,7 +58,8 @@ const Home = () => {
 
             <Box
                 sx={{
-                    width: '700px'
+                    width: '700px',
+                    marginLeft: 10
                 }}
             >
                 <Typography
@@ -34,8 +71,24 @@ const Home = () => {
                         marginBottom: 2
                     }}
                 >
-                    <span style={{ color: '#1F1F1F', margin: '0' }}>Hi, we're</span>
-                    <span style={{ color: '#BBCDFF' }}>Pauss Production</span>
+                    <span style={{ color: '#1F1F1F' }}>
+                        {!firstDone && (
+                            <Typewriter
+                                words={["👋 Hi, we're " ]}
+                                typeSpeed={27}
+                                onType={handleType}
+                            />
+                        )}
+                        {firstDone && "👋 Hi, we're " }
+                    </span>
+                    <span style={{ color: '#BBCDFF' }}>
+                        {firstDone && (
+                            <Typewriter
+                                words={["Pauss Production" ]}
+                                typeSpeed={27}
+                            />
+                        )}
+                    </span>
                 </Typography>
                 <Typography
                     sx={{
@@ -43,7 +96,8 @@ const Home = () => {
                         fontSize: '28px'
                     }}
                 >
-                    We are a small team of friends creating websites, games, and other digital experiences
+                    We are a small team of friends creating 
+                    {/* <WordsTransition words={words} interval={2000}/> */}
                 </Typography>
             </Box>
         </Box>
