ed26dac96d4d0661632a1d567a51cfd14cd35a69
russell <gamingrussellwa@gmail.com>
2025-04-19 19:14:15 +0700

    [Update] Home File | [Update] Index File
    - Added Quicksand font to index.html
    - Styled the Intro part of the homepage

--- pauss-production/src/views/Home.tsx
+++ pauss-production/src/views/Home.tsx
@@ -2,35 +2,52 @@ import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const Home = () => {
-     return (
-          <Box
-               display="flex"
-               justifyContent="space-between"
-               alignItems="center"
-          >
-               <Box
-                    component="img"
-                    alt='pauss logo'
-                    src='/assets/Logo Perusahaan@4x.png'
-                    sx={{
-                         width: '33%'
-                    }}
-               />
+    return (
+        <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            sx={{
+                marginTop: 10
+            }}
+        >
+            <Box
+                component="img"
+                alt='pauss logo'
+                src='/assets/Logo Perusahaan@4x.png'
+                sx={{
+                    width: '33%'
+                }}
+            />
 
-               <Box>
-                    <Typography
-                         sx={{
-                              fontFamily: 'cursive'
-                         }}
-                    >
-                         <span style={{ color: '#1F1F1F' }}>Hi, we're </span>
-                         <span style={{ color: '#BBCDFF' }}>Pauss Production</span>
-                    </Typography>
-                    <Typography>We are a small team of friends creating websites, games, and other digital experiences</Typography>
-                    <p>linked In logo</p>
-               </Box>
-          </Box>
-     )
+            <Box
+                sx={{
+                    width: '700px'
+                }}
+            >
+                <Typography
+                    sx={{
+                        fontFamily: 'Quicksand',
+                        fontWeight: 700,
+                        fontSize: '45px',
+                        marginTop: 10,
+                        marginBottom: 2
+                    }}
+                >
+                    <span style={{ color: '#1F1F1F', margin: '0' }}>Hi, we're</span>
+                    <span style={{ color: '#BBCDFF' }}>Pauss Production</span>
+                </Typography>
+                <Typography
+                    sx={{
+                        fontFamily: 'Poppins',
+                        fontSize: '28px'
+                    }}
+                >
+                    We are a small team of friends creating websites, games, and other digital experiences
+                </Typography>
+            </Box>
+        </Box>
+    )
 }
 
 export default Home;
