8e03c5fc04a7c850c8b0845eb4e646678ffd5235
Russell <gamingrussellwa@gmail.com>
2025-04-19 16:39:33 +0700

    [Update] Home File
    - Added Pauss Logo and some text in Homepage

--- pauss-production/src/Main.tsx
+++ pauss-production/src/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-// import './css/index.css'
+import './css/index.css'
 import Home from './views/Home.tsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
--- pauss-production/src/views/Home.tsx
+++ pauss-production/src/views/Home.tsx
@@ -1,11 +1,35 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const Home = () => {
-     
      return (
-          <div>
-               This is Home
-          </div>
+          <Box
+               display="flex"
+               justifyContent="space-between"
+               alignItems="center"
+          >
+               <Box
+                    component="img"
+                    alt='pauss logo'
+                    src='/assets/Logo Perusahaan@4x.png'
+                    sx={{
+                         width: '33%'
+                    }}
+               />
+
+               <Box>
+                    <Typography
+                         sx={{
+                              fontFamily: 'cursive'
+                         }}
+                    >
+                         <span style={{ color: '#1F1F1F' }}>Hi, we're </span>
+                         <span style={{ color: '#BBCDFF' }}>Pauss Production</span>
+                    </Typography>
+                    <Typography>We are a small team of friends creating websites, games, and other digital experiences</Typography>
+                    <p>linked In logo</p>
+               </Box>
+          </Box>
      )
 }
 
