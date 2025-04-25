e43df33545b457695d88145a46dfe8793ea6717c
willi301 <will301will@gmail.comm>
2025-04-25 14:43:41 -0700

    [Update] projectGallery
    - added descriptions and category tag div

--- pauss-production/src/views/projectGallery/projectGallery.tsx
+++ pauss-production/src/views/projectGallery/projectGallery.tsx
@@ -40,22 +40,61 @@ const ProjectGallery = () => {
                 }}>
 
                 {/*This is one card containing the image and description */}
-                <Box>
+                <Box
+                    width={'40%'}>
                     {/* This is the image */}
                     <Box
                         component="img"
                         src='/assets/aniroulandingpage.jpg'
                         alt='project 1'
-                        width='40%'
+                        width='100%'
                         sx={{
                             borderRadius: '50px',
                             boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)'
                         }}>
                     </Box>
 
-                    <Box>
-
+                    {/* This is the description */}
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        width= '180px'
+                        height= '20px'
+                        marginTop={2}
+                        sx={{
+                            borderRadius: '50px',
+                            border: '1px solid black', 
+                            padding: 2 
+                        }}>
+                        <Typography
+                            sx={{
+                                fontFamily: 'Quicksand',
+                                fontWeight: 700,
+                                color: '#1F1F1F'
+                            }}>
+                            Web Dev
+                        </Typography>
                     </Box>
+
+                    <Typography
+                        variant='h4'
+                            sx={{
+                                fontFamily: 'Quicksand',
+                                fontWeight: 700,
+                                color: '#1F1F1F'
+                            }}>
+                            AniRou
+                    </Typography>
+
+                    <Typography
+                            sx={{
+                                fontFamily: 'Quicksand',
+                                fontWeight: 400,
+                                color: '#1F1F1F'
+                            }}>
+                            An random anime generator website based on your preferred Anime Genre’s
+                    </Typography>
 
                 </Box>
                 
